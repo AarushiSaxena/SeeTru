adecc76835ce7711d31a99b6dae06b7074d5da6f
AarushiSaxena <77536386+AarushiSaxena@users.noreply.github.com>
2022-05-29 22:45:15 +0530

    Update cameratwo.py

--- src/cameratwo.py
+++ src/cameratwo.py
@@ -1,7 +1,3 @@
-# To run the script, execute the following commands
-# workon cv
-# python3 real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
-
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
@@ -16,16 +12,10 @@ from gtts import gTTS
 import pygame
 import threading
 
-
-# def speak(text):#this defines the function SPEAK and creates the command for its use
-#this defines the function SPEAK and creates the command for its use
 tts = gTTS(text="There is an Object", lang="en")
 filename = "voice.mp3"
 tts.save(filename)
-        # audio_file = os.path.dirname(__file__) + '/voice.mp3'
-        # playsound.playsound(audio_file)
-
-
+       
 def pianoF():
 	playsound('voice.mp3')
          #saves the audio file
@@ -102,47 +92,4 @@ class Videotwo(object):
         ret,jpg=cv2.imencode('.jpg',frame)
         return jpg.tobytes()
 
-# # loop over the frames from the video stream
-# while True:
-# 	# resize the video sstream window at a maximum width of 500 pixels
-# 	frame = vs.read()
-# 	frame = imutils.resize(frame, width=500)
-
-# 	# grab the frame dimensions and convert it to a blob
-# 	# Binary Large Object = BLOB
-# 	(h, w) = frame.shape[:2]
-# 	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
-
-# 	# pass the blob through the network and get the detections
-# 	net.setInput(blob)
-# 	detections = net.forward()
-
-# 	# loop over the detections
-# 	for i in np.arange(0, detections.shape[2]):
-# 		# extract the probability of the prediction
-# 		probability = detections[0, 0, i, 2]
-
-# 		# filter out weak detections by ensuring that probability is
-# 		# greater than the min probability
-# 		if probability > args["probability"]:
-# 			# extract the index of the class label from the
-# 			# 'detections', then compute the (x, y)-coordinates of
-# 			# the bounding box for the object
-# 			idx = int(detections[0, 0, i, 1])
-# 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-# 			(startX, startY, endX, endY) = box.astype("int")
-
-# 			# draw the prediction on the frame
-# 			label = "{}: {:.2f}%".format(CLASSES[idx], probability * 100)
-# 			cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[idx], 2)
-# 			y = startY - 15 if startY - 15 > 15 else startY + 15
-# 			cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-# 			t1 = threading.Thread(target=pianoF, args=())
-# 			t1.start()
-# 			t1.join()
-# 			# speak("The value is" + label)
-# 			#check if point is in the first box
-
-
-# 	# show the output frame
 
