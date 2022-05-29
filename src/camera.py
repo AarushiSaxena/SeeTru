60ab103b657616d1733979e995eb58a30e917139
AarushiSaxena <77536386+AarushiSaxena@users.noreply.github.com>
2022-05-29 22:38:21 +0530

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -1,8 +1,4 @@
 import cv2
-
-
-
-
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
@@ -28,15 +24,6 @@ faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 ap = argparse.ArgumentParser()
 ap.add_argument("--mode",help="train/display")
 mode = ap.parse_args().mode
-
-# plots accuracy and loss curves
-
-# def speak(text):#this defines the function SPEAK and creates the command for its use
-#         tts = gTTS(text=text, lang="en")
-#         filename = "voice.mp3"
-#         tts.save(filename)
-#         playsound.playsound(filename)
-#         os.remove(filename) #saves the audio file
 
 def speak(text):#this defines the function SPEAK and creates the command for its use
         tts = gTTS(text=text, lang="en")
@@ -143,57 +130,3 @@ class Video(object):
             
         ret,jpg=cv2.imencode('.jpg',frame)
         return jpg.tobytes()
-
-
-
-# # Disable scientific notation for clarity
-# np.set_printoptions(suppress=True)
-# image = cv2.VideoCapture(0)
-# # Load the model
-# model = tensorflow.keras.models.load_model('keras_model.h5')
-
-# """
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1."""
-# data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-# # A dict that stores the labels
-# labels = gen_labels()
-
-# class Videotwo(object):
-#     def __init__(self):
-#         self.video=cv2.VideoCapture(0)
-#     def __del__(self):
-#         self.video.release()
-#     def get_frame(self):
-#         font = cv2.FONT_HERSHEY_SIMPLEX
-#         ret,frame=self.video.read()
-#         frame = cv2.flip(frame, 1)
-#     # In case the image is not read properly
-#         # if not ret:
-#         #     continue
-            
-#         # Draw a rectangle, in the frame
-#         frame = cv2.rectangle(frame, (220, 80), (530, 360), (0, 0, 255), 3)
-#         # Draw another rectangle in which the image to labelled is to be shown.
-#         frame2 = frame[80:360, 220:530]
-#         # resize the image to a 224x224 with the same strategy as in TM2:
-#         # resizing the image to be at least 224x224 and then cropping from the center
-#         frame2 = cv2.resize(frame2, (224, 224))
-#         # turn the image into a numpy array
-#         image_array = np.asarray(frame2)
-#         # Normalize the image
-#         normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-#         # Load the image into the array
-#         data[0] = normalized_image_array
-#         pred = model.predict(data)
-#         result = np.argmax(pred[0])
-
-#         # Print the predicted label into the screen.
-#         cv2.putText(frame,  "Label : " +
-#                     labels[str(result)], (280, 400), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
-        
-        
-            
-#         ret,jpg=cv2.imencode('.jpg',frame)
-#         return jpg.tobytes()
